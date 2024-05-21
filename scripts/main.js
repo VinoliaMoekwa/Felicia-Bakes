ff4b0a8aa3a09ad9f38bc3df95e9c155093395f1
VinoliaMoekwa <moe23001@byui.edu>
2024-05-21 22:46:27 +0200

    header and footer

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('toggle-button');
+    const navbarLinks = document.getElementById('navbar-links');
+
+    toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active');
+    });
+});
+
+
