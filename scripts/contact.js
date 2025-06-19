9dd863a0accd2190afefe8e558687253228b1684
VinoliaMoekwa <moe23001@byui.edu>
2025-06-20 00:55:23 +0200

    html

--- scripts/contact.js
+++ scripts/contact.js
@@ -8,6 +8,20 @@ let productType = "";
 if (orderInfo.order?.cakeSelected) productType += "Cake ";
 if (orderInfo.order?.cupcakeSelected) productType += "Cupcakes";
 
+let orderDetails = `
+  Product Type: ${productType || "Not provided"}<br>
+  Flavour: ${orderInfo.order.flavour || "Not provided"}<br>
+  Filling: ${orderInfo.order.filling || "Not provided"}<br>
+  Size: ${orderInfo.order.cakeSize || "Not provided"}<br>
+  Design: ${orderInfo.order.designType || "Not provided"}<br>
+  Color Scheme: ${orderInfo.order.colorScheme || "Not provided"}<br>
+  Message: ${orderInfo.order.message || "Not provided"}<br>
+  Dietary: ${orderInfo.order.dietary || "Not provided"}<br>
+  Occasion: ${orderInfo.order.occasion || "Not provided"}<br>
+  Event Date: ${orderInfo.order.eventDate || "Not provided"}<br>
+
+`;
+
 // For WhatsApp (plain text)
 let orderDetailsText = 
   `Product Type: ${productType || "Not provided"}\n` +
