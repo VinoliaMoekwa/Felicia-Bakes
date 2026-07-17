97154816c59f3c4d9b1b4727a8ef3059dea9b73b
Vinolia Moekwa <nmoekwa@byupathway.edu>
2026-07-17 14:29:09 +0200

    responsive issues

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -4,5 +4,6 @@ const navLinks = document.querySelector('.navigation');
 if (hamburger && navLinks) {
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
+    hamburger.setAttribute('aria-expanded', navLinks.classList.contains('show'));
   });
 }
